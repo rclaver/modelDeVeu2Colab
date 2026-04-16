ed88f12d56a4d7f34b17387b577d5be66687068f
rclaver <rclaver@xtec.cat>
2026-04-16 16:11:33 +0200

    avaluació periòdica

--- avaluació_periòdica.sh
+++ avaluació_periòdica.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-p TTS/bin/synthesize.py \
+p /home/rafael/bin/entorns/tts/lib/python3.11/site-packages/TTS/bin/synthesize.py \
   --text "Això és una prova de síntesi de veu en català." \
   --model_path ./proces/tts_output/best_model.pth \
-  --config_path ./config.json \
+  --config_path ./config_mix.json \
   --out_path prova.wav
